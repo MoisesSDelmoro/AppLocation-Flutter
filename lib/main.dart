582a874eca1406801957ec38d27392bad91bce6b
mdelmoro <moisesdelmoro@gec.inatel.br>
2021-08-14 09:51:17 -0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:app/services/location.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'APP location',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LocationApp(),
+    );
+  }
+}
---
+++ lib/services/location.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class LocationApp extends StatelessWidget {
+  Future<List<double>> _locationApp() async {
+    double latitude, longitude;
+    List<double> locationStudent = [];
+    Location location = new Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        //return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        //return;
+      }
+    }
+    _locationData = await location.getLocation();
+
+    latitude = _locationData.latitude!;
+    longitude = _locationData.longitude!;
+
+    locationStudent = [latitude, longitude];
+    print("--------------------------------------------------");
+    print(latitude);
+    print("++++++");
+    print(longitude);
+    print("--------------------------------------------------");
+    return locationStudent;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<double> locationStudent = [];
+    return Container(
+      child: Center(
+        child: Container(
+          child: FlatButton(
+            color: Colors.cyan,
+            onPressed: () {
+              locationStudent = _locationApp() as List<double>;
+              //_openApp();
+            },
+            child: Text(""),
+          ),
+        ),
+      ),
+    );
+  }
+}
